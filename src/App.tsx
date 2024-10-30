b8834cea2058d5a262f8c91e181c250b183e1646
LogicCuteGuy <ohm@logiccuteguy.com>
2024-10-30 15:01:10 +0700

    aaaa

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
+
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+
+            <div className="w-full min-h-screen flex items-center justify-center bg-white relative overflow-hidden" onClick={event => {
+                event.preventDefault();
+                window.open("https://discord.aerina.date/");
+            }}>
+                <img src="poster1.png" alt="poster1"/>
+            </div>
+        </>
+    );
 }
 
 export default App;
